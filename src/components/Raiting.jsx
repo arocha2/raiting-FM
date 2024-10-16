de9fa2bc2934e24be7957c272b1a322e477d3b2f
arocha2 <abda08.ab@gmail.com>
2024-10-16 10:51:37 -0400

    done

--- src/components/Raiting.jsx
+++ src/components/Raiting.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import starIcon from "../assets/images/icon-star.svg";
+import imgThank from "../assets/images/illustration-thank-you.svg";
 
 export const Raiting = () => {
   const [view, setView] = useState(true);
   const [raiting, setRaiting] = useState(null);
   const handleClick = () => {
+    if (raiting == null) return;
     setView(false);
   };
   return (
     <>
       {view ? (
-        <section className="max-w-sm p-8 text-white flex flex-col gap-4 bg-zinc-800 rounded-[30px]">
+        <section
+          className={`max-w-sm p-8 text-white flex flex-col gap-4 bg-zinc-800 rounded-[30px]`}
+        >
           <div className="w-10 h-10 bg-zinc-700 rounded-full flex justify-center items-center">
             <img className="w-4" src={starIcon} alt="icon" />
           </div>
@@ -38,7 +42,27 @@ export const Raiting = () => {
           </button>
         </section>
       ) : (
-        <section className="text-white">{raiting}</section>
+        <section
+          className={` max-w-sm p-8 text-white flex flex-col gap-4 bg-zinc-800 rounded-[30px] `}
+        >
+          <div>
+            <img
+              className="w-36 mx-auto py-2"
+              src={imgThank}
+              alt="img-thanks"
+            />
+            <p className="my-5 mx-auto px-4 py-2 rounded-full text-center text-xs tracking-wider text-Orange bg-zinc-700 w-fit">
+              You selected {raiting} out of 5
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-4">
+            <p className="text-2xl font-bold">Thank you! </p>
+            <p className="text-center text-sm leading-6">
+              We appreciate you taking the time to give a rating. If you ever
+              need more support, don’t hesitate to get in touch!
+            </p>
+          </div>
+        </section>
       )}
     </>
   );
